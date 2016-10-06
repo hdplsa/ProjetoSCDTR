c0d9a7283098fa501a9be8c1ce894798f50e4032
FranciscoFSilva <francisco.silva.e@gmail.com>
2016-10-06 18:10:38 +0100

    Mudança no ficheiro Matlab
    -Inicialização de y
    -mudar a condição de > para ~=

--- lightControl/Arduino_get_data.m
+++ lightControl/Arduino_get_data.m
@@ -3,13 +3,14 @@ arduino = serial('COM3','BaudRate',9600);
 fopen(arduino);
 
 x=1:100;
+y=zeros(1,length(x));
 
 fprintf(arduino,'%i',42); 
 
 for i = 1:length(x)
    
     tmp = fscanf(arduino,'lux = %f\n');
-    if length(tmp)>1
+    if length(tmp)~=1
         continue;
     end
     y(i) = tmp;
