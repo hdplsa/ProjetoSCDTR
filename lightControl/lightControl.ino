92f0addaee077e69803e931a07c0d9584bbaf52f
Hugo Loureiro <hdplsa@gmail.com>
2016-10-09 18:00:45 +0100

    testes

--- lightControl/lightControl.ino
+++ lightControl/lightControl.ino
@@ -1,18 +1,19 @@
 #include "LightSensor.h"
 #include "LedPWM.h"
 #include "LumIntensity.h"
+#include "SerialCom.h"
 
-LightSensor *ls;
-LedPWM *Ledp;
 LumIntensity *luminten;
+SerialCom *serialcom;
 
 const int ledPin = 9;
 const int sensorPin = 5;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  
   luminten = new LumIntensity(ledPin, sensorPin);
+  serialcom = new SerialCom(9600, luminten);
 }
 
 // funções auxiliares
@@ -35,36 +36,38 @@ void reset_board_serial(void (f())){
   }
 }
 
-void reset_led(){
-
-  Ledp->setLedPWM(0); 
-
-  delay(1000);
-
-}
+//void reset_led(){
+//
+//  Ledp->setLedPWM(0); 
+//
+//  delay(1000);
+//
+//}
 
 
 void loop() {
   // put your main code here, to run repeatedly:
 
   double R, lux;
+  char buffer[20];
   
   //Ledp->setLedPWM(100);
-  R = ls->getSensorResistance();
-  lux = luminten->getLuminousItensity();
+  //R = ls->getSensorResistance();
+  //lux = luminten->getLuminousItensity();
 
   //Serial.print("R = ");
   //Serial.print(R);
   //Serial.print("\n");
-  Serial.print("lux = ");
-  Serial.print(lux);
-  Serial.print("\n");
-  Serial.print("Energia = ");
-  Serial.print(Ledp->calculateLedEnergyCycle(),4);
-  Serial.print("\n");
+  sprintf(buffer,"lux = %i \n",16);
+  serialcom->send_message(buffer);
+  //Serial.print(lux);
+  //Serial.print("\n");
+  //Serial.print("Energia = ");
+  //Serial.print(Ledp->calculateLedEnergyCycle(),4);
+  //Serial.print("\n");
 
   delay(50);
 
-  reset_board_serial(reset_led);
-    
+  //reset_board_serial(reset_led);
+
   }
