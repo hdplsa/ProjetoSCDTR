266421d7e8bd0814a66954d0125073b7bf0a6863
Unknown <miguelgameiro94@hotmail.com>
2016-12-31 15:45:35 +0000

    Envio de mensagens de erro para o PI

--- lightControl/Error.h
+++ lightControl/Error.h
@@ -1,9 +1,18 @@
 #ifndef ERROR_H_INCLUDED
 #define ERROR_H_INCLUDED
 
+#include <stdio.h>
+#include "SerialCom.h"
 
 class Error{
-  
+    //Informações sobre o erro
+    int errorcode;
+    char errorstr[100];
+
+    public:
+        Error();
+        void setSerialString(char *str);
+        ~Error();
 };
 
 #endif
