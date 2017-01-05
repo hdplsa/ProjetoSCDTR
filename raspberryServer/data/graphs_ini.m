5b6193d476219abed23c7512aa4f43ad2000cff8
mgameiro <miguelgameiro94@hotmail.com>
2017-01-05 22:09:00 +0000

    Métricas calculadas MATLAB

--- raspberryServer/data/graphs_ini.m
+++ raspberryServer/data/graphs_ini.m
@@ -2,32 +2,32 @@ close all;
 
 fig1 = figure(1);
 subplot(2,1,1);
-plot(time0,ref0,time0,y0,time1,ref1,time1,y1);
+plot(time0/1000,ref0,time0/1000,y0,time1/1000,ref1,time1/1000,y1);
 title('Luminosidade');
 legend('Ref 1','Ilum 1','Ref 2','Ilum 2');
 xlabel('t[s]');
 ylabel('Iluminância [lx]');
 
 subplot(2,1,2);
-plot(time0,duty0,time1,duty1);
+plot(time0/1000,duty0,time1/1000,duty1);
 title('Duty cycle');
 legend('Sistema 2','Sistema 1');
 xlabel('t[s]');
 
 fig2 = figure(2);
 subplot(3,1,1);
-plot(time0,Energy);
+plot(time0/1000,Energy);
 title('Energy');
 xlabel('t[s]');
 
 subplot(3,1,2);
-plot(time0,Cerror);
+plot(time0/1000,Cerror);
 title('Cerror');
 xlabel('t[s]');
 
 subplot(3,1,3);
-plot(time0,Verror);
-title('Vflicker');
+plot(time0/1000,Verror);
+title('Verror');
 xlabel('t[s]');
 
 %% save to file
--- raspberryServer/data/processData.m
+++ raspberryServer/data/processData.m
@@ -22,5 +22,30 @@ Energy = Energy0(1:length(time0));
 Cerror = Cerror0(1:length(time0));
 Verror = Verror0(1:length(time0));
 
+%% Calculo de métricas
+time0 = time0/1000;
+time1 = time1/1000;
+
+%Energia
+Energy = zeros(1,length(time0));
+for n=2:length(time0)
+    for k = 2:n
+        Energy(n) = Energy(n) + (duty0(k-1)/255)*(time0(k)-time0(k-1))+(duty1(k-1)/255)*(time1(k)-time1(k-1));
+    end
+end
+
+%Cerror
+Cerror = zeros(1,length(time0));
+for n=1:length(time0)
+   Cerror(n) = (1/n)*(sum(max(ref0(1:n)-y0(1:n),0))+sum(max(ref1(1:n)-y1(1:n),0))); 
+end
+
+
+%Vflicker
+Vflicker = zeros(1,length(time0));
+for n=3:length(time0)
+   Vflicker(n) = (1/(n*0.02^2))*(abs(ref0(n)-2*ref0(n-1)+ref0(n-2))+abs(ref1(n)-2*ref1(n-1)+ref1(n-2)));
+end
+
 %% Graphs from files
 graphs;
